61f93ef0c98b8879333c2c0fc2f6cae9bdc32f79
kapilan <km2717@imperial.ac.uk>
2019-01-27 11:13:28 +0000

    fixed duplicated playlist obj

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React, { Component, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const APIurl = "https://crowdbeats-host.herokuapp.com/";
+// const APIurl = "https://crowdbeats-host.herokuapp.com/";
+const APIurl = "https://localhost:8888/";
+
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {
       partyCode:'',
       authFinished: false,
-      playlists: []
+      playlists: [],
+      searchQuery:'',
+      searchResults: []
     }
   }
 
@@ -17,6 +21,25 @@ class App extends Component {
     this.setState({partyCode:event.target.value})
   }
   
+  updateSearchQuery(event){
+    this.setState({searchQuery:event.target.value})
+  }
+
+  submitSearchQuery(){
+    fetch(APIurl+"search?party_id="+this.state.partyCode+"search="+this.state.searchQuery)
+    .then(function(response) {
+      // The response is a Response instance.
+      // You parse the data into a useable format using `.json()`
+      return response.json();
+    })
+    .then(
+      (response) => {
+        this.setState
+      }
+
+        )
+      }
+  }
 
   submitPartyCode(){
     fetch(APIurl+"newguest?party_id="+this.state.partyCode)
@@ -60,7 +83,7 @@ class App extends Component {
       <div className="App">
       {!this.state.authFinished ? 
       <Fragment>
-         <h1>Please Enter Pary Code.</h1>
+         <h1>Please Enter Party Code.</h1>
           <input placeholder="Party Code" value={this.state.partyCode} onChange={(event) => this.updatePartyCode(event)} />
           <button onClick={() => {this.submitPartyCode()}}>Submit</button>
       </Fragment>
@@ -74,6 +97,9 @@ class App extends Component {
           </div>
         ))
         }
+
+            <input placeholder="Search" value={this.state.searchQuery} onChange={(event) => this.updatePartyCode(event)} />
+            <button onClick={() => {this.submitSearchQuery()}}>Submit</button>
         </Fragment>
     }
          
